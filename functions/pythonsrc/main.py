cef89a747f6db457274c9e5e0fa3194bf6a72e97
Hatoma Daiya <phaosdove@gmail.com>
2019-10-30 23:23:24 +0900

    recipeを獲得するためのCategoryを獲得した

--- functions/pythonsrc/main.py
+++ functions/pythonsrc/main.py
@@ -7,6 +7,7 @@ import time
 import os
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def getRecipeTweet(event, context):
 
@@ -86,16 +87,38 @@ def getRecipeTweet(event, context):
 
 # CookPadからぶっこ抜く
 def getMaterials(event, context):
-    
-    target_url = 'https://cookpad.com/recipe/3988130'
+    recipe_id_list = context
+    base_url = 'https://cookpad.com/recipe/'
+    for recipe_id in recipe_id_list:
+        r = requests.get(base_url + str(recipe_id))
+        soup = BeautifulSoup(r.text, 'lxml')
+        ingredients = soup.find_all('div', class_='ingredient_name')
+        kigou = []
+        for ingredient in ingredients:
+            # 記号文字を抜きたい
+            material = re.sub("[^ぁ-んァ-ンーa-zA-Z0-9一-龠０-９\-\r]", '', ingredient.text)
+            print(material)
+
+def getRecipeUrl():
+    target_url = 'https://cookpad.com/category/10'
     r = requests.get(target_url)
     soup = BeautifulSoup(r.text, 'lxml')
-    ingredients = soup.find_all('div', class_='ingredient_name')
-    for ingredient in ingredients:
-        print(ingredient.text)
+    # recipes = soup.find_all('a', href=re.compile("*/recipe/d{7}"))
+    recipes = soup.find_all('a', href=re.compile("/recipe/\d{7}"))
+    recipe_id_list = list(map(lambda recipe: re.search("\d{7}", recipe['href']).group(), recipes))
+    getMaterials('hoge', recipe_id_list)
 
+def getCategoryId():
+    target_url = 'https://cookpad.com/category/list'
+    r = requests.get(target_url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    sub_category = soup.find_all('a', href=re.compile("/category/\d+"))
+    category_id_list = list(map(lambda category: re.search("\d+", category['href']).group(), sub_category))
+    print(list(set(category_id_list)))
 
 if __name__ == "__main__":
-    getMaterials('hoge', 'fuga')
+    # getMaterials('hoge', 'fuga')
+    # getRecipeUrl()
+    getCategoryId()
 
     
